28ee0086c3fcd424e4d158f0f5b2272957b371e4
Anandhakrishnan15 <127764237+Anandhakrishnan15@users.noreply.github.com>
2023-12-18 12:35:48 +0530

    Delete project1 directory

--- project1/config/config.js
+++
@@ -1,4 +0,0 @@
-const PORT = 3333;
-// const Database= "mongodb+srv://anandhakrishnanvellat15:anandhakrishnan@db.eimhhz6.mongodb.net/DB?retryWrites=true&w=majority"
-// const Database="mongodb://localhost:27017/project1";
-module.exports= {PORT}
